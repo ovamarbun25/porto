d077a85de65b6e6246bbd8bd64d51a808b12b01b
Marbun, Ova <ova.marbun@accenture.com>
2023-03-29 16:13:01 +0700

    minor update

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -10,7 +10,7 @@ import ReactJSLogo from "../public/assets/skills/react.png";
 import Laravel from "../public/assets/skills/laravel.png";
 import SQL from "../public/assets/skills/sql.png";
 import Tailwind from "../public/assets/skills/tailwind.png";
-
+import Link from "next/link";
 
 const Skills = () => {
   return (
@@ -22,149 +22,124 @@ const Skills = () => {
         <h2 className="py-4">What I Can Do</h2>
 
         <div className="grid grid-cols-2 lg:grid-cols-4 gap-8">
-          <div className="p-6 shadow-xl rounded-xl hover:scale-110 ease-in duration-300">
-            <div className="grid grid-cols-2 gap-4 justify-center items-center">
-              <div className="m-auto">
-                <Image
-                  src={HTML}
-                  alt="/"
-                  width="64"
-                  height="64"
-                />
-              </div>
-              <div className="flex flex-col items-center justify-center">
-                <h3>HTML</h3>
-              </div>
-            </div>
-          </div>
 
-          <div className="p-6 shadow-xl rounded-xl hover:scale-110 ease-in duration-300">
-            <div className="grid grid-cols-2 gap-4 justify-center items-center">
-              <div className="m-auto">
-                <Image
-                  src={CSS}
-                  alt="/"
-                  width="64"
-                  height="64"
-                />
-              </div>
-              <div className="flex flex-col items-center justify-center">
-                <h3>CSS</h3>
+          <Link href="https://developer.mozilla.org/en-US/docs/Learn/Getting_started_with_the_web/HTML_basics" target="_blank">
+            <div className="p-6 shadow-xl rounded-xl hover:scale-110 ease-in duration-300 h-24">
+              <div className="grid grid-cols-2 gap-4 justify-center items-center">
+                <div className="m-auto">
+                  <Image src={HTML} alt="/" width="64" height="64" />
+                </div>
+                <div className="flex flex-col items-center justify-center">
+                  <h3>HTML</h3>
+                </div>
               </div>
             </div>
-          </div>
+          </Link>
 
-          <div className="p-6 shadow-xl rounded-xl hover:scale-110 ease-in duration-300">
-            <div className="grid grid-cols-2 gap-4 justify-center items-center">
-              <div className="m-auto">
-                <Image
-                  src={Javascript}
-                  alt="/"
-                  width="64"
-                  height="64"
-                />
-              </div>
-              <div className="flex flex-col items-center justify-center">
-                <h3>Javascript</h3>
+          <Link href="https://developer.mozilla.org/en-US/docs/Web/CSS" target="_blank">
+            <div className="p-6 shadow-xl rounded-xl hover:scale-110 ease-in duration-300 h-24">
+              <div className="grid grid-cols-2 gap-4 justify-center items-center">
+                <div className="m-auto">
+                  <Image src={CSS} alt="/" width="64" height="64" />
+                </div>
+                <div className="flex flex-col items-center justify-center">
+                  <h3>CSS</h3>
+                </div>
               </div>
             </div>
-          </div>
+          </Link>
 
-          <div className="p-6 shadow-xl rounded-xl hover:scale-110 ease-in duration-300">
-            <div className="grid grid-cols-2 gap-4 justify-center items-center">
-              <div className="m-auto">
-                <Image
-                  src={PHP}
-                  alt="/"
-                  width="64"
-                  height="64"
-                />
-              </div>
-              <div className="flex flex-col items-center justify-center">
-                <h3>PHP</h3>
+          <Link href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
+            <div className="p-6 shadow-xl rounded-xl hover:scale-110 ease-in duration-300 h-24">
+              <div className="grid grid-cols-2 gap-4 justify-center items-center">
+                <div className="m-auto">
+                  <Image src={Javascript} alt="/" width="64" height="64" />
+                </div>
+                <div className="flex flex-col items-center justify-center">
+                  <h3>Javascript</h3>
+                </div>
               </div>
             </div>
-          </div>
+          </Link>
 
-          <div className="p-6 shadow-xl rounded-xl hover:scale-110 ease-in duration-300">
-            <div className="grid grid-cols-2 gap-4 justify-center items-center">
-              <div className="m-auto">
-                <Image
-                  src={NextJS}
-                  alt="/"
-                  width="64"
-                  height="64"
-                />
-              </div>
-              <div className="flex flex-col items-center justify-center">
-                <h3>NextJS</h3>
+          <Link href="https://www.php.net/" target="_blank">
+            <div className="p-6 shadow-xl rounded-xl hover:scale-110 ease-in duration-300 h-24">
+              <div className="grid grid-cols-2 gap-4 justify-center items-center">
+                <div className="m-auto">
+                  <Image src={PHP} alt="/" width="64" height="64" />
+                </div>
+                <div className="flex flex-col items-center justify-center">
+                  <h3>PHP</h3>
+                </div>
               </div>
             </div>
-          </div>
+          </Link>
 
-          <div className="p-6 shadow-xl rounded-xl hover:scale-110 ease-in duration-300">
-            <div className="grid grid-cols-2 gap-4 justify-center items-center">
-              <div className="m-auto">
-                <Image
-                  src={ReactJSLogo}
-                  alt="/"
-                  width="64"
-                  height="64"
-                />
-              </div>
-              <div className="flex flex-col items-center justify-center">
-                <h3>ReactJS</h3>
+          <Link href="https://nextjs.org/" target="_blank">
+            <div className="p-6 shadow-xl rounded-xl hover:scale-110 ease-in duration-300 h-24">
+              <div className="grid grid-cols-2 gap-4 justify-center items-center">
+                <div className="m-auto">
+                  <Image src={NextJS} alt="/" width="64" height="64" />
+                </div>
+                <div className="flex flex-col items-center justify-center">
+                  <h3>NextJS</h3>
+                </div>
               </div>
             </div>
-          </div>
+          </Link>
 
-          <div className="p-6 shadow-xl rounded-xl hover:scale-110 ease-in duration-300">
-            <div className="grid grid-cols-2 gap-4 justify-center items-center">
-              <div className="m-auto">
-                <Image
-                  src={Laravel}
-                  alt="/"
-                  width="64"
-                  height="64"
-                />
-              </div>
-              <div className="flex flex-col items-center justify-center">
-                <h3>Laravel</h3>
+          <Link href="https://react.dev/" target="_blank">
+            <div className="p-6 shadow-xl rounded-xl hover:scale-110 ease-in duration-300 h-24">
+              <div className="grid grid-cols-2 gap-4 justify-center items-center">
+                <div className="m-auto">
+                  <Image src={ReactJSLogo} alt="/" width="64" height="64" />
+                </div>
+                <div className="flex flex-col items-center justify-center">
+                  <h3>ReactJS</h3>
+                </div>
               </div>
             </div>
-          </div>
+          </Link>
 
-          <div className="p-6 shadow-xl rounded-xl hover:scale-110 ease-in duration-300">
-            <div className="grid grid-cols-2 gap-4 justify-center items-center">
-              <div className="m-auto">
-                <Image
-                  src={SQL}
-                  alt="/"
-                  width="64"
-                  height="64"
-                />
-              </div>
-              <div className="flex flex-col items-center justify-center">
-                <h3>SQL</h3>
+          <Link href="https://laravel.com/" target="_blank">
+            <div className="p-6 shadow-xl rounded-xl hover:scale-110 ease-in duration-300 h-24">
+              <div className="grid grid-cols-2 gap-4 justify-center items-center">
+                <div className="m-auto">
+                  <Image src={Laravel} alt="/" width="64" height="64" />
+                </div>
+                <div className="flex flex-col items-center justify-center">
+                  <h3>Laravel</h3>
+                </div>
               </div>
             </div>
-          </div>
+          </Link>
 
-          <div className="p-6 shadow-xl rounded-xl hover:scale-110 ease-in duration-300">
-            <div className="grid grid-cols-2 gap-4 justify-center items-center">
-              <div className="m-auto">
-                <Image
-                  src={Tailwind}
-                  alt="/"
-                  width="64"
-                  height="64"
-                />
-              </div>
-              <div className="flex flex-col items-center justify-center">
-                <h3>Tailwind CSS</h3>
+          <Link href="https://www.w3schools.com/sql/" target="_blank">
+            <div className="p-6 shadow-xl rounded-xl hover:scale-110 ease-in duration-300 h-24">
+              <div className="grid grid-cols-2 gap-4 justify-center items-center">
+                <div className="m-auto">
+                  <Image src={SQL} alt="/" width="64" height="64" />
+                </div>
+                <div className="flex flex-col items-center justify-center">
+                  <h3>SQL</h3>
+                </div>
               </div>
             </div>
-          </div>
+          </Link>
+
+          <Link href="https://tailwindcss.com/" target="_blank">
+            <div className="p-6 shadow-xl rounded-xl hover:scale-110 ease-in duration-300 h-24">
+              <div className="grid grid-cols-2 gap-4 justify-center items-center">
+                <div className="m-auto">
+                  <Image src={Tailwind} alt="/" width="64" height="64" />
+                </div>
+                <div className="flex flex-col items-center justify-center">
+                  <h3>Tailwind CSS</h3>
+                </div>
+              </div>
+            </div>
+          </Link>
+
         </div>
       </div>
     </div>
